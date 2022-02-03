d904c3d128500813a688d1991186fd7c608aee3e
Heinz Schloemer <heinz.schloemer@gmx.net>
2022-02-03 14:24:58 +0100

    deleted CommandLineRunner from BackendApplication (was necessary to get 2 test user in DB

--- backend/src/main/java/de/neuefische/backend/BackendApplication.java
+++ backend/src/main/java/de/neuefische/backend/BackendApplication.java
@@ -13,29 +13,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.List;
 
 @SpringBootApplication
-public class BackendApplication implements CommandLineRunner {
+public class BackendApplication {
 
-    @Autowired
-    private MongoUserRepository repository;
-
-    @Autowired
-    private PasswordEncoder encoder;
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-
-        //test add a user
-        String encodedPassword = encoder.encode("heinz1234");
-        repository.save(new UserMongo("heinz", encodedPassword, "heinz.test@test.de", List.of(new SimpleGrantedAuthority(MongoUserDetailsService.AUTHORITY_API_READWRITE))));
-        encodedPassword = encoder.encode("heinz12345");
-        repository.save(new UserMongo("heinz2", encodedPassword, "heinz.test@test.de", List.of(new SimpleGrantedAuthority(MongoUserDetailsService.AUTHORITY_API_READWRITE))));
-        //test show user
-        System.out.println("ALL USERS");
-        System.out.println("-------------");
-        System.out.println(repository.findAll());
-    }
 }
