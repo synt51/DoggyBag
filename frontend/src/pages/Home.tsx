029dd9e68038c11dd48f3a440eb7b765b303185e
Heinz Schloemer <heinz.schloemer@gmx.net>
2022-02-04 23:35:53 +0100

    inserted demo button for new marker

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,10 +6,24 @@ export default function Home() {
 
     return (
         <div className="homePage">
-            <div className="title">
-                <h1>Getting bags</h1>
+            <div className="upper-box">
+                <div className="title">
+                    <h1>Getting bags</h1>
+                </div>
+                {/*shows the current position with doggy bag markers (ALWAYS) */}
+                <Button className="locationButton" variant="contained" color="success">
+                    Get current location
+                </Button>
             </div>
-            <Button className="locationButton" variant="contained" color="success">Get current location </Button>
+            <div className="lower-box">
+                <div className="map">
+                    <h1>MAP HERE</h1>
+                </div>
+                {/*creates new doggy bag marker (ONLY IF LOGGED IN) */}
+                <Button className="markerButton" variant="contained" color="success">
+                    Mark new Doggy Bag Place
+                </Button>
+            </div>
         </div>
     )
 }
