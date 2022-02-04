059968b8a976a091cc0f90b0246a126e1ab66421
Heinz Schloemer <heinz.schloemer@gmx.net>
2022-02-04 23:50:32 +0100

    implemented some location data (not finished)

---
+++ frontend/src/components/GoogleLocation.tsx
@@ -0,0 +1,7 @@
+import {useState} from "react";
+
+export default function GoogleLocation () {
+
+    const [latitude, setLatitude] = useState<string>("")
+    const [longitude, setLongitude] = useState<string>("")
+}
