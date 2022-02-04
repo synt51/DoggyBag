95743d40e61f1cba342db8ca3d1a650b2a219149
Heinz Schloemer <heinz.schloemer@gmx.net>
2022-02-04 21:39:46 +0100

    implemented current location button

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,15 @@
+import {Button} from "@mui/material";
+import './Home.scss';
+
 export default function Home() {
 
 
     return (
-        <div >
-            <h1>Getting bags</h1>
+        <div className="homePage">
+            <div className="title">
+                <h1>Getting bags</h1>
+            </div>
+            <Button className="locationButton" variant="contained" color="success">Get current location </Button>
         </div>
     )
 }
