d2bef2cf2376250d98a9d8d2ad3def6985feadf3
Heinz Schloemer <heinz.schloemer@gmx.net>
2022-02-06 14:12:17 +0100

    implemented openstreetmap

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,10 @@
 import {Button} from "@mui/material";
 import './Home.scss';
+import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
+
+
+const DEFAULT_LATITUDE =48;
+const DEFAULT_LANGITUDE =-123;
 
 export default function Home() {
 
@@ -17,7 +22,17 @@ export default function Home() {
             </div>
             <div className="lower-box">
                 <div className="map">
-                    <h1>MAP HERE</h1>
+                    <MapContainer center={[50.941278, 6.958281]} zoom={13}>
+                        <TileLayer
+                            attribution={'&copy; <a href="https://www.openstreetmap.org/copyright">OpenTestStreetMap</a> contributors'}
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[50.941278, 6.958281]}>
+                            <Popup>
+                                A pretty CSS3 popup. <br /> Easily customizable.
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
                 </div>
                 {/*creates new doggy bag marker (ONLY IF LOGGED IN) */}
                 <Button className="markerButton" variant="contained" color="success">
