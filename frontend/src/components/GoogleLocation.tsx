e8b6e95f924cada95c77b2b6ece3b7493c479c06
Heinz Schloemer <heinz.schloemer@gmx.net>
2022-02-08 11:51:49 +0100

    get Current Location is working

--- frontend/src/components/GoogleLocation.tsx
+++
@@ -1,7 +0,0 @@
-import {useState} from "react";
-
-export default function GoogleLocation () {
-
-    const [latitude, setLatitude] = useState<string>("")
-    const [longitude, setLongitude] = useState<string>("")
-}
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,47 @@
 import {Button} from "@mui/material";
 import './Home.scss';
-import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
+import {MapContainer, TileLayer, Marker} from "react-leaflet";
+import L from "leaflet";
+import React, {useCallback, useEffect, useState} from "react";
+import useGeoLocation from "../hooks/useGeoLocation";
+import {Language} from "@mui/icons-material";
 
 
-const DEFAULT_LATITUDE =48;
-const DEFAULT_LANGITUDE =-123;
+const markerIcon = new L.Icon({
+    iconUrl: require("../resources/images/marker.png"),
+    iconSize: [40, 40],
+    iconAnchor: [17, 46], //[left/right, top/bottom]
+    popupAnchor: [0, -46], //[left/right, top/bottom]
+});
 
 export default function Home() {
+
+    const location = useGeoLocation();
+    const [map, setMap] = useState<L.Map>();
+    const [center, setCenter] = useState({lat: 50.941278, lng: 6.958281});
+    const ZOOM_LEVEL_DEFAULT = 13;
+    const ZOOM_LEVEL_CURRENT = 16;
+
+    const showLocation = useCallback(() => {
+        if (location.coordinates && map) {
+            map.flyTo(location.coordinates, ZOOM_LEVEL_CURRENT)
+            console.log(location.coordinates)
+        }
+    }, [map, location])
+
+
+    const onMove = useCallback(() => {
+        if (map?.getCenter()) {
+            setCenter(map?.getCenter())
+        }
+    }, [map])
+
+    useEffect(() => {
+        map?.on('move', onMove)
+        return () => {
+            map?.off('move', onMove)
+        }
+    }, [map, onMove])
 
 
     return (
@@ -16,22 +51,28 @@ export default function Home() {
                     <h1>Getting bags</h1>
                 </div>
                 {/*shows the current position with doggy bag markers (ALWAYS) */}
-                <Button className="locationButton" variant="contained" color="success">
+                <Button className="locationButton" variant="contained" color="success" endIcon={<Language/>}
+                        onClick={showLocation}>
                     Get current location
                 </Button>
             </div>
             <div className="lower-box">
                 <div className="map">
-                    <MapContainer center={[50.941278, 6.958281]} zoom={13}>
+                    <MapContainer center={center} zoom={ZOOM_LEVEL_DEFAULT} whenCreated={setMap}>
                         <TileLayer
-                            attribution={'&copy; <a href="https://www.openstreetmap.org/copyright">OpenTestStreetMap</a> contributors'}
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            attribution={'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}
+                            url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
                         />
-                        <Marker position={[50.941278, 6.958281]}>
-                            <Popup>
-                                A pretty CSS3 popup. <br /> Easily customizable.
-                            </Popup>
-                        </Marker>
+
+                        {location.loaded && !location.error && location.coordinates && (
+                            <Marker
+                                icon={markerIcon}
+                                position={[
+                                    location.coordinates.lat,
+                                    location.coordinates.lng,
+                                ]}
+                            ></Marker>
+                        )}
                     </MapContainer>
                 </div>
                 {/*creates new doggy bag marker (ONLY IF LOGGED IN) */}
