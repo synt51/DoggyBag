927465c50bf1255ef8dd170cffadd9adde2f0cce
Heinz Schloemer <heinz.schloemer@gmx.net>
2022-02-08 16:32:23 +0100

    deleted react import and mui import

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import {
     Toolbar,
     Typography,
     IconButton,
-    Avatar, Container, Menu, MenuItem, Button, Tooltip,
+    Avatar
 } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import {useNavigate} from "react-router-dom";
@@ -13,156 +13,156 @@ import React from "react";
 
 export default function NavBar() {
 
-    // const navigate = useNavigate()
-    //
-    // const handleAvatar = () => {
-    //     navigate('/login')
-    // }
-    //
-    // return (
-    //     <Box sx={{flexGrow: 1}}>
-    //         <AppBar className="appBar" position="static" color="transparent">
-    //             <Toolbar>
-    //                 <IconButton
-    //                     size="large"
-    //                     edge="start"
-    //                     color="inherit"
-    //                     aria-label="menu"
-    //                     sx={{mr: 2}}
-    //                 >
-    //                     <MenuIcon/>
-    //                 </IconButton>
-    //                 <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-    //                     DoggyBag
-    //                 </Typography>
-    //                 <Avatar className="avatar" onClick={handleAvatar}/>
-    //             </Toolbar>
-    //         </AppBar>
-    //     </Box>
-    // );
+    const navigate = useNavigate()
 
-    const pages = ['Getting bags', 'Finest routes', 'MedDog'];
-    const settings = ['Profile', 'Account', 'Logout'];
+    const handleAvatar = () => {
+        navigate('/login')
+    }
 
-        const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-        const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-
-        const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-            setAnchorElNav(event.currentTarget);
-        };
-        const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-            setAnchorElUser(event.currentTarget);
-        };
-
-        const handleCloseNavMenu = () => {
-            setAnchorElNav(null);
-        };
-
-        const handleCloseUserMenu = () => {
-            setAnchorElUser(null);
-        };
-
-        return (
-            <AppBar position="static" color="inherit">
-                <Container maxWidth="xl">
-                    <Toolbar disableGutters>
-                        <Typography
-                            variant="h6"
-                            noWrap
-                            component="div"
-                            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-                        >
-                            DoggyBag
-                        </Typography>
-
-                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                            <IconButton
-                                size="large"
-                                aria-label="account of current user"
-                                aria-controls="menu-appbar"
-                                aria-haspopup="true"
-                                onClick={handleOpenNavMenu}
-                                color="inherit"
-                            >
-                                <MenuIcon />
-                            </IconButton>
-                            <Menu
-                                id="menu-appbar"
-                                anchorEl={anchorElNav}
-                                anchorOrigin={{
-                                    vertical: 'bottom',
-                                    horizontal: 'left',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'left',
-                                }}
-                                open={Boolean(anchorElNav)}
-                                onClose={handleCloseNavMenu}
-                                sx={{
-                                    display: { xs: 'block', md: 'none' },
-                                }}
-                            >
-                                {pages.map((page) => (
-                                    <MenuItem key={page} onClick={handleCloseNavMenu}>
-                                        <Typography textAlign="center">{page}</Typography>
-                                    </MenuItem>
-                                ))}
-                            </Menu>
-                        </Box>
-                        <Typography
-                            variant="h6"
-                            noWrap
-                            component="div"
-                            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-                        >
-                            DoggyBag
-                        </Typography>
-                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                            {pages.map((page) => (
-                                <Button
-                                    key={page}
-                                    onClick={handleCloseNavMenu}
-                                    sx={{ my: 2, color: 'white', display: 'block' }}
-                                >
-                                    {page}
-                                </Button>
-                            ))}
-                        </Box>
-
-                        <Box sx={{ flexGrow: 0 }}>
-                            <Tooltip title="Open settings">
-                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                                </IconButton>
-                            </Tooltip>
-                            <Menu
-                                sx={{ mt: '45px' }}
-                                id="menu-appbar"
-                                anchorEl={anchorElUser}
-                                anchorOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                open={Boolean(anchorElUser)}
-                                onClose={handleCloseUserMenu}
-                            >
-                                {settings.map((setting) => (
-                                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                        <Typography textAlign="center">{setting}</Typography>
-                                    </MenuItem>
-                                ))}
-                            </Menu>
-                        </Box>
-                    </Toolbar>
-                </Container>
+    return (
+        <Box sx={{flexGrow: 1}}>
+            <AppBar className="appBar" position="static" color="transparent">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{mr: 2}}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                        DoggyBag
+                    </Typography>
+                    <Avatar className="avatar" onClick={handleAvatar}/>
+                </Toolbar>
             </AppBar>
-        );
+        </Box>
+    );
+
+    // const pages = ['Getting bags', 'Finest routes', 'MedDog'];
+    // const settings = ['Profile', 'Account', 'Logout'];
+    //
+    //     const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+    //     const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+    //
+    //     const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    //         setAnchorElNav(event.currentTarget);
+    //     };
+    //     const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    //         setAnchorElUser(event.currentTarget);
+    //     };
+    //
+    //     const handleCloseNavMenu = () => {
+    //         setAnchorElNav(null);
+    //     };
+    //
+    //     const handleCloseUserMenu = () => {
+    //         setAnchorElUser(null);
+    //     };
+    //
+    //     return (
+    //         <AppBar position="static" color="inherit">
+    //             <Container maxWidth="xl">
+    //                 <Toolbar disableGutters>
+    //                     <Typography
+    //                         variant="h6"
+    //                         noWrap
+    //                         component="div"
+    //                         sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+    //                     >
+    //                         DoggyBag
+    //                     </Typography>
+    //
+    //                     <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+    //                         <IconButton
+    //                             size="large"
+    //                             aria-label="account of current user"
+    //                             aria-controls="menu-appbar"
+    //                             aria-haspopup="true"
+    //                             onClick={handleOpenNavMenu}
+    //                             color="inherit"
+    //                         >
+    //                             <MenuIcon />
+    //                         </IconButton>
+    //                         <Menu
+    //                             id="menu-appbar"
+    //                             anchorEl={anchorElNav}
+    //                             anchorOrigin={{
+    //                                 vertical: 'bottom',
+    //                                 horizontal: 'left',
+    //                             }}
+    //                             keepMounted
+    //                             transformOrigin={{
+    //                                 vertical: 'top',
+    //                                 horizontal: 'left',
+    //                             }}
+    //                             open={Boolean(anchorElNav)}
+    //                             onClose={handleCloseNavMenu}
+    //                             sx={{
+    //                                 display: { xs: 'block', md: 'none' },
+    //                             }}
+    //                         >
+    //                             {pages.map((page) => (
+    //                                 <MenuItem key={page} onClick={handleCloseNavMenu}>
+    //                                     <Typography textAlign="center">{page}</Typography>
+    //                                 </MenuItem>
+    //                             ))}
+    //                         </Menu>
+    //                     </Box>
+    //                     <Typography
+    //                         variant="h6"
+    //                         noWrap
+    //                         component="div"
+    //                         sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+    //                     >
+    //                         DoggyBag
+    //                     </Typography>
+    //                     <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+    //                         {pages.map((page) => (
+    //                             <Button
+    //                                 key={page}
+    //                                 onClick={handleCloseNavMenu}
+    //                                 sx={{ my: 2, color: 'white', display: 'block' }}
+    //                             >
+    //                                 {page}
+    //                             </Button>
+    //                         ))}
+    //                     </Box>
+    //
+    //                     <Box sx={{ flexGrow: 0 }}>
+    //                         <Tooltip title="Open settings">
+    //                             <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+    //                                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+    //                             </IconButton>
+    //                         </Tooltip>
+    //                         <Menu
+    //                             sx={{ mt: '45px' }}
+    //                             id="menu-appbar"
+    //                             anchorEl={anchorElUser}
+    //                             anchorOrigin={{
+    //                                 vertical: 'top',
+    //                                 horizontal: 'right',
+    //                             }}
+    //                             keepMounted
+    //                             transformOrigin={{
+    //                                 vertical: 'top',
+    //                                 horizontal: 'right',
+    //                             }}
+    //                             open={Boolean(anchorElUser)}
+    //                             onClose={handleCloseUserMenu}
+    //                         >
+    //                             {settings.map((setting) => (
+    //                                 <MenuItem key={setting} onClick={handleCloseUserMenu}>
+    //                                     <Typography textAlign="center">{setting}</Typography>
+    //                                 </MenuItem>
+    //                             ))}
+    //                         </Menu>
+    //                     </Box>
+    //                 </Toolbar>
+    //             </Container>
+    //         </AppBar>
+    //     );
 
 }
