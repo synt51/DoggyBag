795b8c7d38eb8b0c3a25900068fa3d67b59584bc
Heinz Schloemer <heinz.schloemer@gmx.net>
2022-02-11 11:31:52 +0100

    predesigned meddog page

--- frontend/src/pages/MedDog.tsx
+++ frontend/src/pages/MedDog.tsx
@@ -1,13 +1,31 @@
 import './MedDog.scss';
+import {LocalizationProvider, StaticDatePicker} from "@mui/lab";
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import {TextField} from "@mui/material";
+import React from "react";
 
-export default function MedDog(){
+export default function MedDog() {
 
+    const [value, setValue] = React.useState<Date | null>(new Date());
 
     return (
         <div className="medDog">
             <div className="title">
                 <h1>MedDog</h1>
                 <h4>Your time companion</h4>
+            </div>
+            <div className="calendar">
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <StaticDatePicker
+                        displayStaticWrapperAs="desktop"
+                        openTo="year"
+                        value={value}
+                        onChange={(newValue) => {
+                            setValue(newValue);
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                    />
+                </LocalizationProvider>
             </div>
         </div>
     )
