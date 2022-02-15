393e6184f541d075b52e72efda2c8656e2a58497
Heinz Schloemer <heinz.schloemer@gmx.net>
2022-02-15 17:19:16 +0100

    deleted format in backend

--- backend/src/main/java/de/neuefische/backend/model/AppointmentCreationDTO.java
+++ backend/src/main/java/de/neuefische/backend/model/AppointmentCreationDTO.java
@@ -1,6 +1,5 @@
 package de.neuefische.backend.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +12,5 @@ import java.util.Date;
 public class AppointmentCreationDTO {
 
     private String appointmentName;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="dd-MM-yyyy")
     private Date endDate;
 }
