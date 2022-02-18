697e53ca201b7ba8411986fd98ab7616cec9acd3
Heinz Schloemer <heinz.schloemer@gmx.net>
2022-02-18 17:45:36 +0100

    bagplacecounter component to show how many BagPlaces are created by the user himself

---
+++ frontend/src/components/BagPlaceCounter.tsx
@@ -0,0 +1,21 @@
+import BagPlace from "../models/BagPlace";
+import bagPlaceLogo from "../resources/images/logo.png";
+import "./BagPlaceCounter.scss"
+
+
+interface BagPlacesProps{
+    bagPlaces: BagPlace[]
+}
+export default function BagPlaceCounter(props: BagPlacesProps){
+
+    const {bagPlaces} = props
+
+    const bagPlaceCount = bagPlaces.length;
+
+    return(
+        <div className="bagPlaceCounter">
+            <p>{bagPlaceCount}</p>
+            <img src={bagPlaceLogo} alt={""}/>
+        </div>
+    )
+}
